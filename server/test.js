64379cc354541b2897e91da4e91b99282f1c7527
Jigyanshu Don <jigyanshukumawat@gmail.com>
2025-01-01 12:15:18 +0530

    server folder added

---
+++ server/test.js
@@ -0,0 +1,8 @@
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+
+// Example usage:
+const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArray = removeDuplicates(arrayWithDuplicates);
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
