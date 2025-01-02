3cd4d33bf03c348beeb60f55c012d2945d34e7eb
Dishant Kumawat <dishantk9799@gmail.com>
2025-01-02 21:05:25 +0530

    user controller done

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
-import dotenv from "dotenv";
+ import dotenv from "dotenv";
 import express from "express"; // Import Express
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import connectDB from "./database/db.js";
-
+import userRoute from "./routes/user.route.js"
 dotenv.config({
   path: "./.env",
 });
 
 const app = express(); // Initialize Express app
+//default middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: `http://localhost:${port}`,
+  credentials: true,
+}));
+//apis
+app.use("/api/v1/user",userRoute);
 
 connectDB()
   .then(() => {
